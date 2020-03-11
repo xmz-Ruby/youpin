bb0500ad802d9258ece570a2d45e45aad719bf5d
xmz-Ruby <774246121@qq.com>
2020-03-11 20:26:50 +0800

    Updates

---
+++ precache-manifest.25fde231ba65c6f09ad1677749d70218.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758dbffef4fb660a84d699f5682f3ace",
+    "url": "/youpin/index.html"
+  },
+  {
+    "revision": "9227fcaee492e783703c",
+    "url": "/youpin/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6165d2af5bbb2b94b540",
+    "url": "/youpin/static/css/main.620b77f4.chunk.css"
+  },
+  {
+    "revision": "9227fcaee492e783703c",
+    "url": "/youpin/static/js/2.fe50884c.chunk.js"
+  },
+  {
+    "revision": "57223b1e9d50dc7c340c5d54029a7988",
+    "url": "/youpin/static/js/2.fe50884c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6165d2af5bbb2b94b540",
+    "url": "/youpin/static/js/main.460927e7.chunk.js"
+  },
+  {
+    "revision": "2721b71a9f0d0fb5ff2f",
+    "url": "/youpin/static/js/runtime-main.4e390984.js"
+  },
+  {
+    "revision": "28695494fea80c06e5d717a8e19460f3",
+    "url": "/youpin/static/media/tv.28695494.jpg"
+  }
+]);
